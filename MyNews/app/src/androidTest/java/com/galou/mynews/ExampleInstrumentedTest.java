4af54ec49e006c3dce1436583847cb7ce6ab25fd
GalouM <galou.minisini@gmail.com>
2019-03-16 12:01:12 -0700

    new test

--- MyNews/app/src/androidTest/java/com/galou/mynews/ExampleInstrumentedTest.java
+++ MyNews/app/src/androidTest/java/com/galou/mynews/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.galou.mynews", appContext.getPackageName());
+        assertEquals("com.galou.mynews.debug", appContext.getPackageName());
     }
 }
