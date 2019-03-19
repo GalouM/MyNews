4272237d9074809f714d3460e3ad874b79053ee0
GalouM <galou.minisini@gmail.com>
2019-03-19 14:08:26 -0700

    toolbar in search activity
    need to create layout for toolbar

--- MyNews/app/src/main/java/com/galou/mynews/controllers/activities/SearchActivity.java
+++ MyNews/app/src/main/java/com/galou/mynews/controllers/activities/SearchActivity.java
@@ -1,15 +1,30 @@
 package com.galou.mynews.controllers.activities;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.galou.mynews.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class SearchActivity extends AppCompatActivity {
+
+    @BindView(R.id.search_activity_toolbar) Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        ButterKnife.bind(this);
+        this.configureToolbar();
+    }
+
+    private void configureToolbar(){
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 }
