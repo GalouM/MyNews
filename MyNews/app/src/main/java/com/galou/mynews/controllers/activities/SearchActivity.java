f17b1162a715ba438d89be61bbd70c331b516b85
GalouM <galou.minisini@gmail.com>
2019-03-19 19:30:30 -0700

    imlemented search fragment in activity

--- MyNews/app/src/main/java/com/galou/mynews/controllers/activities/SearchActivity.java
+++ MyNews/app/src/main/java/com/galou/mynews/controllers/activities/SearchActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 import com.galou.mynews.R;
+import com.galou.mynews.controllers.fragments.SearchFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,17 +15,35 @@ public class SearchActivity extends AppCompatActivity {
 
     @BindView(R.id.toolbar) Toolbar toolbar;
 
+    private SearchFragment searchFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         ButterKnife.bind(this);
         this.configureToolbar();
+        this.configureAndShowSearchFragment();
     }
+
+    // -------------------
+    // CONFIGURATION
+    // -------------------
 
     private void configureToolbar(){
         setSupportActionBar(toolbar);
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void configureAndShowSearchFragment(){
+        searchFragment = (SearchFragment) getSupportFragmentManager().findFragmentById(R.id.search_activity_frame_layout);
+        if (searchFragment == null){
+            searchFragment = new SearchFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.search_activity_frame_layout, searchFragment)
+                    .commit();
+        }
+
     }
 }
--- MyNews/app/src/test/java/com/galou/mynews/SearchActivityUnitTest.java
+++ MyNews/app/src/test/java/com/galou/mynews/SearchActivityUnitTest.java
@@ -1,6 +1,7 @@
 package com.galou.mynews;
 
 import com.galou.mynews.controllers.activities.SearchActivity;
+import com.galou.mynews.controllers.fragments.SearchFragment;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -8,6 +9,7 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 
+import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 /**
@@ -26,5 +28,10 @@ public class SearchActivityUnitTest {
     @Test
     public void activityShouldNotBeNull()throws Exception{
         assertNotNull(activity);
+    }
+
+    @Test
+    public void searchFragmentShouldBeVisible() throws Exception {
+        assertEquals(activity.getSupportFragmentManager().findFragmentById(R.id.search_activity_frame_layout).getClass().getName(), SearchFragment.class.getName());
     }
 }
